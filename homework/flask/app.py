20610b6d8c9ef0d8248d915db48fc77c2c27f544
kitty <katiagolub56@gmail.com>
2021-10-20 20:59:12 +0300

    Created web calculator

---
+++ homework/flask/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def greetings():
+    return render_template("greetings.html")
+
+
+@app.route("/calc/<int:x>/<int:y>/<string:operator>")
+def calc(x, y, operator):
+    if operator == "sum" or operator == "+":
+        return render_template("calc.html", x=x, y=y, operator="+", result=x + y)
+    elif operator == "sub" or operator == "-":
+        return render_template("calc.html", x=x, y=y, operator="-", result=x + y)
+    elif operator == 'div' or operator == '/':
+        if y == 0:
+            return 'Divide by zero'
+        else:
+            return render_template('calc.html', x=x, y=y, operator='/', result=x / y)
+    elif operator == 'mult' or operator == '*':
+        return render_template('calc.html', x=x, y=y, operator='*', result=x * y)
+    else:
+        return 'Please choose a correct operation!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
