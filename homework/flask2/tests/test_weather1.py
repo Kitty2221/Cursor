4e7f80cd10d62d46df3f8e9ae0b6461380c44fb4
kitty <katiagolub56@gmail.com>
2021-10-26 17:24:59 +0300

    Unittest get-your-weather

---
+++ homework/flask2/tests/test_weather1.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, main as unittest_main
+from app import app
+
+
+class PlaylistsTests(TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        app.config["Testing"] = True
+
+    def test_weather(self):
+        result = self.client.get('/weather?city=Kiev')
+        self.assertEqual(result.status, '200 OK')
+        page_content = result.get_data(as_text=True)
+        self.assertIn("Kiev", page_content)
+
+
+if __name__ == '__main__':
+    unittest.main()
