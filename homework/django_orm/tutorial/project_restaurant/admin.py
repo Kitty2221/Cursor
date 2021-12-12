b3803d5c40013468700975ae39625d7251eceb38
kitty <katiagolub56@gmail.com>
2021-12-12 11:37:33 +0200

    Registered Restaurant models

---
+++ homework/django_orm/tutorial/project_restaurant/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from project_restaurant.models import Restaurant, Dish, Staff, City, Country, Menu
+
+admin.site.register(Restaurant)
+admin.site.register(Dish)
+admin.site.register(Staff)
+admin.site.register(City)
+admin.site.register(Country)
+admin.site.register(Menu)
